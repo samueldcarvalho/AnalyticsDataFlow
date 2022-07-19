cfec67a72b6e33b0cdf7914ce23cbda41829c425
samueldcarvalho <samueldcarvalho99@gmail.com>
2022-07-19 17:25:26 -0300

    Criação de classe abstrata de DataRepository e classe VendaRepository

---
+++ src/Producer/AnalyticsDataFlow.Producer.Infrastructure/Repositories/DataRepository.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Configuration;
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnalyticsDataFlow.Producer.Infrastructure.Repositories
+{
+    public abstract class DataRepository
+    {
+        protected readonly MySqlConnection _connection;
+
+        public DataRepository(IConfiguration configuration)
+        {
+            string connectionString = configuration.GetConnectionString("DefaultConnection");
+            if (string.IsNullOrWhiteSpace(connectionString))
+                throw new Exception("ConnectionString não declarada no AppSettings");
+
+            _connection = new MySqlConnection(connectionString);
+        }
+    }
+}
---
+++ src/Producer/AnalyticsDataFlow.Producer.Infrastructure/Repositories/VendaRepository.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnalyticsDataFlow.Producer.Infrastructure.Repositories
+{
+    public class VendaRepository : DataRepository
+    {
+        public VendaRepository(IConfiguration configuration) : base(configuration) { }
+    }
+}
