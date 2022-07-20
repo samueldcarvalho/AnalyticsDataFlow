c2f5dc9dca1d3996ba86305100a4b565471b55ec
samueldcarvalho <samueldcarvalho99@gmail.com>
2022-07-20 16:04:24 -0300

    Criação do Consumer com ApacheSpark e conteinerização usando Docker da infra do Kafka com Zookeeper e ControlCenter da Confluentic

---
+++ src/Consumer/AnalyticsDataFlow.Consumer.Application/Services/ConsumerService.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.Spark;
+using Microsoft.Spark.Sql;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AnalyticsDataFlow.Consumer.Application.Services
+{
+    public class ConsumerService : BackgroundService
+    {
+        private readonly ILogger<ConsumerService> _logger;
+
+        public ConsumerService(ILogger<ConsumerService> logger)
+        {
+            _logger = logger;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            var spark = SparkSession.Builder().GetOrCreate();
+
+            while (true)
+            {
+                if (stoppingToken.IsCancellationRequested)
+                    break;
+
+                var df = spark.ReadStream()
+                    .Format("kafka")
+                    .Option("kafka.bootstrap.servers", "localhost:9092")
+                    .Option("subscribe", "venda_topic")
+                    .Option("startingOffsets", "earliest")
+                    .Load();
+
+                df.PrintSchema();
+
+                spark.Stop();
+            }   
+        }
+    }
+}
--- src/Producer/AnalyticsDataFlow.Producer.Application/Services/ProducerService.cs
+++ src/Producer/AnalyticsDataFlow.Producer.Application/Services/ProducerService.cs
@@ -1,6 +1,7 @@
 ﻿using AnalyticsDataFlow.Producer.Application.Interfaces;
 using AnalyticsDataFlow.Producer.Application.Models;
 using Confluent.Kafka;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
@@ -8,7 +9,6 @@ using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -20,10 +20,16 @@ namespace AnalyticsDataFlow.Producer.Application.Services
         private readonly ILogger<ProducerService> _logger;
         private IVendaRepository _vendaRepository;
 
+        private readonly string _brokerURL;
         private int _counter = 0;
 
-        public ProducerService(IServiceProvider serviceProvider, ILogger<ProducerService> logger)
+        public ProducerService(IServiceProvider serviceProvider, IConfiguration configuration, ILogger<ProducerService> logger)
         {
+            _brokerURL = configuration.GetSection("Kafka:ServerURL").Value;
+
+            if (string.IsNullOrWhiteSpace(_brokerURL))
+                throw new Exception("BrokerURL não foi informado nas varíaveis de ambiente.");
+
             _serviceProvider = serviceProvider;
             _logger = logger;
         }
@@ -40,9 +46,9 @@ namespace AnalyticsDataFlow.Producer.Application.Services
 
         private async Task Produce(CancellationToken cancellationToken)
         {
-            var config = new ProducerConfig { BootstrapServers = "dev001.softwaredrogaria.com.br:9000" };
+            var config = new ProducerConfig { BootstrapServers = _brokerURL };
 
-            using (var prod = new ProducerBuilder<Null, string>(config).Build())
+            using (var prod = new ProducerBuilder<string, string>(config).Build())
             {
                 while (true)
                 {
@@ -63,14 +69,14 @@ namespace AnalyticsDataFlow.Producer.Application.Services
 
                             foreach (IEnumerable<Venda> pacote in pacotesVendas)
                             {
-                                var result = await prod.ProduceAsync("topico_vendas", new Message<Null, string>() { Value = JsonConvert.SerializeObject(pacote)});
+                                var result = await prod.ProduceAsync("venda_topic", new Message<string, string>() { Key= $"{pacote.Min(v => v.Id)}{pacote.Max(v => v.Id)}", Value = JsonConvert.SerializeObject(pacote)});;
                                 Console.WriteLine($"Pacote [{pacote.Min(v => v.Id)}-{pacote.Max(v => v.Id)}] para '{result.TopicPartitionOffset}' | DataPacote: {new DateTime(2020, 11, 01).AddDays(_counter)}");
                             }
                         }
 
                         _counter++;
 
-                        await Task.Delay(5000);
+                        await Task.Delay(10000);
                     }
                     catch(Exception ex)
                     {
