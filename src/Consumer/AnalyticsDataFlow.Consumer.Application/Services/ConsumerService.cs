936bd4ea9b4853ea169a298fe295a04519ec6b01
samueldcarvalho <samueldcarvalho99@gmail.com>
2022-07-21 15:49:16 -0300

    Criação de consumer com Dotnet for Apache Spark

--- src/Consumer/AnalyticsDataFlow.Consumer.Application/Services/ConsumerService.cs
+++ src/Consumer/AnalyticsDataFlow.Consumer.Application/Services/ConsumerService.cs
@@ -1,7 +1,11 @@
-﻿using Microsoft.Extensions.Hosting;
+﻿using AnalyticsDataFlow.Producer.Application.Models;
+using Confluent.Kafka;
+using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.Spark;
 using Microsoft.Spark.Sql;
+using Newtonsoft.Json;
+using Raven.Client.Documents;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -22,24 +26,47 @@ namespace AnalyticsDataFlow.Consumer.Application.Services
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
-            var spark = SparkSession.Builder().GetOrCreate();
-
-            while (true)
+            var config = new ConsumerConfig
             {
-                if (stoppingToken.IsCancellationRequested)
-                    break;
+                BootstrapServers = "localhost:9092",
+                GroupId = "Venda.Consumer",
+                AutoOffsetReset = AutoOffsetReset.Earliest
+            };
 
-                var df = spark.ReadStream()
-                    .Format("kafka")
-                    .Option("kafka.bootstrap.servers", "localhost:9092")
-                    .Option("subscribe", "venda_topic")
-                    .Option("startingOffsets", "earliest")
-                    .Load();
+            var dbConnection = new DocumentStore
+            {
+                Urls = new[] { "http://localhost:8080" },
+                Database = "analytics"
+            }.Initialize();
 
-                df.PrintSchema();
+            using (var consumer = new ConsumerBuilder<Ignore, string>(config).Build())
+            {
+                consumer.Subscribe("venda_topic");
 
-                spark.Stop();
-            }   
+                using (var dbSession = dbConnection.OpenSession())
+                    while (!stoppingToken.IsCancellationRequested)
+                    {
+                        Console.WriteLine($"{DateTime.Now.Hour:00}:{DateTime.Now.Minute:00}:{DateTime.Now.Second:00}:{DateTime.Now.Millisecond:000}");
+
+                        ConsumeResult<Ignore, string> consumeResult = consumer.Consume(stoppingToken);
+
+                        var value = consumeResult?.Message?.Value;
+
+                        if (string.IsNullOrWhiteSpace(value))
+                            continue;
+
+                        var vendas = JsonConvert.DeserializeObject<List<VendaViewModel>>(value);
+
+                        vendas.ForEach(v => dbSession.Store(v));
+
+                        dbSession.SaveChanges();
+                    }
+
+                await Task.Delay(5000, stoppingToken);
+                consumer.Close();
+            }
         }
     }
 }
+
+
---
+++ src/Test/MyKafkaSparkApp/Program.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Spark.Sql;
+using System;
+
+namespace MyKafkaSparkApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SparkSession spark = SparkSession
+                .Builder().AppName("word_count_sample")
+                .Config("spark.jars.packages", "org.apache.spark:spark-sql-kafka-0-10_2.12:3.0.1").Master("local[*]")
+                .GetOrCreate();
+
+            spark.SparkContext.SetLogLevel("ERROR");
+
+            DataFrame dataFrame = spark.ReadStream()
+                .Format("kafka")
+                .Option("kafka.bootstrap.servers", "localhost:9092")
+                .Option("subscribe", "venda_topic")
+                .Option("startingOffsets", "earliest")
+                .Load().SelectExpr("CAST(value AS STRING)");
+
+            var query = dataFrame.WriteStream()
+               .Format("console")
+               .Start();
+
+            query.AwaitTermination();
+
+            dataFrame.Show();
+
+            spark.Stop();
+        }
+    }
+}
